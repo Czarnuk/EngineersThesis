87b11a68d62db8d3f067658cbf5f4ab69d4c3af6
Robert <robdar987@gmail.com>
2023-08-23 21:55:03 +0200

    Api authentication basis

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,30 @@
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    //static class is a class from which we can use inside methods with no initialization
+    public static class ApplicationServiceExtensions
+    {
+        //IServiceCollection is what we will return. First parameter for AddApplicationServices is what we will extend, we want to use IConfiguration parameter also
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<DataContext>(opt => 
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors();
+            /*
+            We have 3 options about the lifetime of how long do we want this service to be available for.
+            1.) AddTransient - token service would be created and disposed within the request as soon as it's used and finished with. Too short for me. AddTransient lives only when was created by framework or controller and disposed when finished his work so a little faster that AddScoped.
+            2.) AddScoped - As a request hit the endpoint, we create controller so the framework instantiates a new instance of that controller. The controller look at it's dependencies or the framework does and need to create these services and create new instances of these services when the controller's created. When the controlles is diposed of at the end of HTTP request, then any dependent services are also disposed. Tha's how long in AddScoped this service lives.
+            3.) AddSingleton - This service's instantiated when the application first starts and is never disposed until the application has closed down. it will work in this case but we would have a service hanging around in memory that we're not actually using on a consistent basis. We only need the token service when we need to create a token, and we don't need to have that token service residing in memory for the entire lenght of the application lifetime.
+            */
+            services.AddScoped<ITokenService, TokenService>();
+
+            return services;
+        }
+    }
+}
---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,36 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+        {
+            /*
+            I'm going to specify the type of authentication scheme that I'm going to be using inside this AddAuthentication parameter. To use this parameter JwtBearerDefaults.AuthenticationScheme I need nuget package called Microsoft.ASPNETCore.authentication.jwtbearer.
+            Next step is to add JwtBearer token with some options for this method. That's how we define options: options.TokenValidationParameters = new TokenValidationParameters
+            Below we specify all of the rules about how our server should validate token as a good token.
+            ValidateIssuerSigningKey = true,                                                                        - this means that our server is going to check the token signing key and make sure it's valid based upon the issuer signing key, which will specify next. With no true value it won't check our token has been signed by the issuer and that means anybody can make up any random token as long as it's a JWT, then our server would accept it.
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])), - here we need to specify what our issuer signing key is 
+            ValidateIssuer = false,                                                                                 - the issuer of the token is of course my API server, but we'd need to pass that information down with the token in order to validate it. If we would have multiple issuers we should set true.
+            ValidateAudience = false                                                                                - the same as property below. That's information that we have not passed with our token
+            */
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => 
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+            
+            return services;
+        }
+    }
+    
+}
